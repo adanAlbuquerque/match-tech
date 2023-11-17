770b1559c92039465973ffbc5ac9101bd5a16b79
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-11-17 11:05:44 -0300

    Commit

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 import Link from "next/link";
+
+import * as $ from './styles';
+
 
 export default function LoginPage() {
   return (
     <>
-      <title>Página de Login!</title>
+      <title>Fazer login nas Contas do Google</title>
+
+      <section>
+      <div>
+        <img src="../images/googleimg.png" alt="Google icon" />
+        <h1>Fazer login</h1>
+        <h3>Use sua Conta do Google</h3>
+      </div>
 
       <form>
-      <div>
-        <input typeof="text" placeholder="Email ou telefone"/>
-      </div>
+      <$.Container>
+        <$.Input type="text" placeholder="Email ou telefone"/>
+      </$.Container>
       <div>
       <ul>
         <li>
@@ -17,10 +28,14 @@ export default function LoginPage() {
       </ul>        
       </div>
 
+      <p>Não está no seu computador? Use uma janela privada para fazer login. Saiba mais</p>
+
       <div>
         <button type="submit">Avançar</button>
       </div>
       </form>
+
+      </section>
 
       
     </>
