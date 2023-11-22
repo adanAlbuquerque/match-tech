b3e094f21d9da84cbdb9595c6a60984b5bfb782e
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-11-22 10:56:46 -0300

    cmmit

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Link from "next/link";
-
 import * as $ from './styles';
-
 
 export default function LoginPage() {
   return (
@@ -10,37 +8,33 @@ export default function LoginPage() {
       <title>Fazer login nas Contas do Google</title>
 
       <$.Body>
-      <$.Content>
+        <$.Content>
 
-        <img src="../images/googleimg.png" alt="Google icon" />
-        <h1>Fazer login</h1>
-        <h3>Use sua Conta do Google</h3>          
+          <img src="../images/googleimg.png" alt="Google icon" />
+          <h1>Fazer login</h1>
+          <h3>Use sua Conta do Google</h3>
 
+          <form>
+            <$.Container>
+              <$.Input type="text" placeholder="Email ou telefone" />
+            </$.Container>
 
-      <form>
-      <$.Container>
-        <$.Input type="text" placeholder="Email ou telefone"/>
-      </$.Container>
-      <div>
-      <button type="button">Esqueceu seu e-mail?</button>
-      </div>
+            <div>
+              <button type="button">Esqueceu seu e-mail?</button>
+            </div>
 
-    <div>
-      Não está no seu computador? Use uma janela privada para fazer login. <a href="https://support.google.com/accounts?p=signin_privatebrowsing&amp;hl=pt-BR" target="_blank" aria-label="Saiba mais sobre como usar o Modo visitante">Saiba mais</a>
-    </div>
+            <p>
+              Não está no seu computador? Use uma janela privada para <br/>fazer login. <a href="https://support.google.com/accounts?p=signin_privatebrowsing&amp;hl=pt-BR" target="_blank" aria-label="Saiba mais sobre como usar o Modo visitante">Saiba mais</a>
+            </p>
 
+            <$.ContainerBotoes>
+              <Link href='signup'>Criar conta</Link>
+              <$.BotaoProxima type="submit">Avançar</$.BotaoProxima>
+            </$.ContainerBotoes>
+          </form>
 
-      <div>
-        <Link href='signup'>Criar conta</Link>
-        <button type="submit">Próxima</button>
-      </div>
-      </form>
-
-      </$.Content>
-
-              
+        </$.Content>
       </$.Body>
-
     </>
-  )
+  );
 }
--- src/app/(public)/login/styles.ts
+++ src/app/(public)/login/styles.ts
@@ -3,35 +3,57 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const Content = styled.div`
 
-img{
+export const BotaoProxima = styled.button`
+  font-family: "Google Sans","Roboto", Arial, sans-serif;
+  background-color: rgb(26,115,232);
+  color: #fff;
+  font-size: 14px;
+  font-weight: 500;
+  padding: 9px 24px 8px 24px;
+  border: none; 
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 
-}
-h1{
-  font-size: 24px;
-  font-weight: 400;
-}
-h3{
-  font-size: 16px;
-  font-weight: 400;
-}
+  &:hover {
+    background-color: #3c78d8;
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  }`;
 
-button{
-  border: none;
-  color: rgb(26,115,232);
-  background-color: transparent;
-  font-size: inherit;
-  text-align: left;
-}
+export const ContainerBotoes = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
 
-
-font-family: "Google Sans","Noto Sans Myanmar UI",arial,sans-serif;
-text-align: center;
-border: 1px solid #D3D3D3;
-border-radius: 8px;
-padding: 20vh 0px;
 `;
+
+export const Content = styled.div`
+  font-family: "Google Sans","Noto Sans Myanmar UI",arial,sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center; 
+  padding: 30px;
+  height: 70vh;
+  border: 1px solid #d3d3d3;
+  border-radius: 8px;
+  background-color: #fff;
+
+  img {
+    width: 92px;
+    margin-bottom:10% ;
+  }
+
+  h1 {
+    font-size: 24px;
+    font-weight: 400;
+  }
+
+  h3 {
+    font-size: 16px;
+    font-weight: 400;
+  }`;
 
 export const Body = styled.body`
   display: flex;
@@ -39,12 +61,20 @@ export const Body = styled.body`
   justify-content: center;
   height: 100vh;
   background-color: #FFF;
-`
+  div:fist-chield
+`;
 
 export const Input = styled.input`
-  border-radius: 4px;
-  color: rgb(32,33,36);
+  border: 1px solid #dfe1e5; 
+  border-radius: 8px;
+  color: #3c4043; 
   font-size: 16px;
-  height: 28px;
-  padding: 13px 45px;
+  height: 40px; 
+  padding: 10px 16px;
+  margin-bottom: 10px; 
+
+  &:focus {
+    border-color: #4285f4;
+    outline: none; 
+  }
 `;
