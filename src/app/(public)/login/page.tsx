33470b5da33451f2d086fd9782f9410295ff1588
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-11-22 21:13:01 -0300

    Comitei

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -8,16 +8,16 @@ export default function LoginPage() {
       <title>Fazer login nas Contas do Google</title>
 
       <$.Body>
-        <$.Content>
+        <$.Section>
 
           <img src="../images/googleimg.png" alt="Google icon" />
           <h1>Fazer login</h1>
           <h3>Use sua Conta do Google</h3>
 
-          <form>
-            <$.Container>
-              <$.Input type="text" placeholder="Email ou telefone" />
-            </$.Container>
+          <$.Form>
+            <div>
+              <input type="text" placeholder="Email ou telefone" />
+            </div>
 
             <div>
               <button type="button">Esqueceu seu e-mail?</button>
@@ -27,13 +27,13 @@ export default function LoginPage() {
               Não está no seu computador? Use uma janela privada para <br/>fazer login. <a href="https://support.google.com/accounts?p=signin_privatebrowsing&amp;hl=pt-BR" target="_blank" aria-label="Saiba mais sobre como usar o Modo visitante">Saiba mais</a>
             </p>
 
-            <$.ContainerBotoes>
-              <Link href='signup'>Criar conta</Link>
-              <$.BotaoProxima type="submit">Avançar</$.BotaoProxima>
-            </$.ContainerBotoes>
-          </form>
 
-        </$.Content>
+          </$.Form>
+            <div>
+              <Link href='signup'>Criar conta </Link>
+              <input type="submit" value="Avançar"/>
+            </div>
+        </$.Section>
       </$.Body>
     </>
   );
--- src/app/(public)/login/styles.ts
+++ src/app/(public)/login/styles.ts
@@ -1,48 +1,90 @@
 import styled from 'styled-components';
 
-
 export const Container = styled.div``;
 
+export const Form = styled.form`
+  p {
+    font-size: 14px;
+    color: #666;
+    margin-top: 20px;
 
-export const BotaoProxima = styled.button`
-  font-family: "Google Sans","Roboto", Arial, sans-serif;
-  background-color: rgb(26,115,232);
-  color: #fff;
-  font-size: 14px;
-  font-weight: 500;
-  padding: 9px 24px 8px 24px;
-  border: none; 
-  border-radius: 4px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+    a {
+      color: rgb(26, 115, 232);
+      text-decoration: none;
+      font-weight: 500;
+      letter-spacing: 0.25px;
+    }
+  }
 
-  &:hover {
-    background-color: #3c78d8;
-    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
-  }`;
+  input {
+    border: 1px solid #dfe1e5;
+    text-align: left;
+    border-radius: 6px;
+    color: #3c4043;
+    font-size: 16px;
+    width: 90%;
+    height: 35px;
+    padding: 10px 15px;
+    margin-bottom: 8px;
 
-export const ContainerBotoes = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 15px;
+    &:focus {
+      border-color: #4285f4;
+      outline: none;
+    }
+  }
+
+  div:first-child {
+    margin-top: 15px;
+    justify-content: flex-end;
+    width: 100%;
+
+    button {
+      background-color: transparent;
+      border: none;
+      color: #4285f4;
+      cursor: pointer;
+      font-size: 14px;
+
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+
+  div:nth-child(2) {
+    margin: -2px -3px;
+    display: flex;
+    justify-content: flex-start;
+
+    button {
+      font-weight: 500;
+      letter-spacing: 0.25px;
+      background-color: transparent;
+      border: none;
+      color: #4285f4;
+      cursor: pointer;
+      font-size: 14px;
+
+      &:hover {
+        text-decoration: none;
+      }
+    }}
 
 `;
 
-export const Content = styled.div`
-  font-family: "Google Sans","Noto Sans Myanmar UI",arial,sans-serif;
+export const Section = styled.section`
+  font-family: "Google Sans", "Noto Sans Myanmar UI", arial, sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center; 
   padding: 30px;
-  height: 70vh;
   border: 1px solid #d3d3d3;
   border-radius: 8px;
   background-color: #fff;
 
   img {
     width: 92px;
-    margin-bottom:10% ;
+    margin-bottom: 10%;
   }
 
   h1 {
@@ -53,28 +95,32 @@ export const Content = styled.div`
   h3 {
     font-size: 16px;
     font-weight: 400;
-  }`;
+  }
+
+  div:nth-child(5){
+      input {
+      font-family: "Google Sans", "Roboto", Arial, sans-serif;
+      background-color: rgb(26, 115, 232);
+      color: #fff;
+      font-size: 14px;
+      font-weight: 500;
+      padding: 9px 24px 8px 24px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+
+      &:hover {
+        background-color: #3c78d8;
+        box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+    }}
+  }
+`;
 
 export const Body = styled.body`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color: #FFF;
-  div:fist-chield
-`;
-
-export const Input = styled.input`
-  border: 1px solid #dfe1e5; 
-  border-radius: 8px;
-  color: #3c4043; 
-  font-size: 16px;
-  height: 40px; 
-  padding: 10px 16px;
-  margin-bottom: 10px; 
-
-  &:focus {
-    border-color: #4285f4;
-    outline: none; 
-  }
+  background-color: #fff;
 `;
